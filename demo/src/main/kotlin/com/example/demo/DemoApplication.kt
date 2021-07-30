d6817591dc99ac262e37037503d2a6b7fca45991
smile1980ja <smile.twitter.follow@gmail.com>
2021-07-30 21:21:14 +0000

    use data jdbc

--- demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,11 +1,21 @@
 package com.example.demo
 
+import org.springframework.data.annotation.Id
+//import org.springframework.data.annotation.PersistenceConstructor
+
+import org.springframework.data.repository.CrudRepository
+
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 import org.springframework.stereotype.Component
 import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.ApplicationArguments
+//import org.springframework.beans.factory.annotation.Autowired
+
+data class Member(@Id val id: String, val name: String = "unset") 
+
+interface MemberRepository : CrudRepository<Member, String> 
 
 @SpringBootApplication
 class DemoApplication 
@@ -13,10 +23,11 @@ fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
 }
 
-
 @Component
-class DemoApplicationRunner: ApplicationRunner {
+class DemoApplicationRunner(val repository : MemberRepository): ApplicationRunner {
+
 	override fun run(args: ApplicationArguments) {
-		println("Hello, Spring!");	
+		val count = repository.count()
+		println("the count is ${count}")
 	}	
 }
