5e6a3e51468df2f803f4edb86081555cee19c3c2
smile1980ja <smile.twitter.follow@gmail.com>
2021-07-30 00:30:38 +0000

    add Hello World

--- demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -3,9 +3,20 @@ package com.example.demo
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
-class DemoApplication
+import org.springframework.stereotype.Component
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.ApplicationArguments
 
+@SpringBootApplication
+class DemoApplication 
 fun main(args: Array<String>) {
 	runApplication<DemoApplication>(*args)
+}
+
+
+@Component
+class DemoApplicationRunner: ApplicationRunner {
+	override fun run(args: ApplicationArguments) {
+		println("Hello, Spring!");	
+	}	
 }
