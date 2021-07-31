5b57542db4df2587b8d85aa84a64101499cdc8dc
smile1980ja <smile.twitter.follow@gmail.com>
2021-07-31 01:50:26 +0000

    add custom query

--- demo/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ demo/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -2,6 +2,7 @@ package com.example.demo
 
 import org.springframework.data.annotation.Id
 //import org.springframework.data.annotation.PersistenceConstructor
+//import org.springframework.data.annotation.Query
 
 import org.springframework.data.repository.CrudRepository
 
@@ -16,6 +17,12 @@ data class Member(@Id val id: String, val name: String = "unset")
 
 interface MemberRepository : CrudRepository<Member, String> 
 
+data class Name(val name: String)
+
+interface NameRepository : CrudRepository<Name, String> {
+   fun getLength(name: String): Long 
+}
+
 @SpringBootApplication
 class DemoApplication 
 fun main(args: Array<String>) {
@@ -23,10 +30,10 @@ fun main(args: Array<String>) {
 }
 
 @Component
-class DemoApplicationRunner(val repository : MemberRepository): ApplicationRunner {
+class DemoApplicationRunner(val repository : NameRepository): ApplicationRunner {
 
 	override fun run(args: ApplicationArguments) {
-		val count = repository.count()
-		println("the count is ${count}")
+		val len = repository.getLength("kenta")
+		println("the count is ${len}")
 	}	
 }
